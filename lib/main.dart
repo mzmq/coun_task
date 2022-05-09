2f67de8e143a30b37293db31b23679e9a33ea3df
Mohammad AL-Qannas <44414204+mzmq@users.noreply.github.com>
2022-05-09 16:30:24 +0300

    new pro

--- lib/main.dart
+++ lib/main.dart
@@ -7,76 +7,74 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-
-
-
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  Future<dynamic> fetchCount ()async{
 
+  Future<dynamic> getData() async {
 // get API used http
-    const url = 'https://countriesnow.space/api/v0.1/countries/info?returns=currency,flag,unicodeFlag,dialCode';
-    final res =await http.get(Uri.parse(url)) ;
+    const urlApi =
+        'https://countriesnow.space/api/v0.1/countries/info?returns=currency,flag,unicodeFlag,dialCode';
+    final getApi = await http.get(Uri.parse(urlApi));
 
-    if(res.statusCode == 200 ){
-      var obj = json.decode(res.body);
-      return await obj;
-
-    }else {
-      return Exception('Error') ;
+    if (getApi.statusCode == 200) {
+      var bodyApi = json.decode(getApi.body);
+      return await bodyApi;
+    } else {
+      return print('Error not found api');
     }
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Get Data Used API'),
-      ),
-      body: FutureBuilder(
-        future:fetchCount() ,
-        builder: (conn ,AsyncSnapshot snap ){
-          if(snap.hasData){
-          return  ListView.builder(
-            itemCount: snap.data['data'].length,
-              itemBuilder: (conn  , index)=> Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(children: [
-                  Text(snap.data['data'][index]['name'] ?? '') ,
-                  Text(snap.data['data'][index]['currency'] ?? '' ) ,
-                ]),
-                SizedBox(
-                  width: 15,
-                ),
-              SvgPicture.network(
-                  snap.data['data'][index]['flag'] ?? '',
-                width: 50,
-              )
-              //  Text(snap.data['data'][0]['flag']) ,
-              ],
-            )) ;
-          }else {
-            return Text('wait') ;
-          }
-
-
-        } ,
-      )
-    );
+        appBar: AppBar(
+          title: Text('assessment coding exercises'),
+        ),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (conn, AsyncSnapshot snap) {
+            if (snap.hasData) {
+              return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) => Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    padding: const EdgeInsets.all(2.0),
+                    child: GridTile(
+                        footer: GridTileBar(
+                          backgroundColor: Colors.black38,
+                          title: Text(snap.data['data'][index]['name'] ?? ''),
+                          trailing: Text(snap.data['data'][index]['currency'] ?? '' , style: TextStyle(
+                            color: Colors.white
+                          )),
+                        ),
+                            child: SvgPicture.network(
+                          snap.data['data'][index]['flag'] ?? 'https://flagicons.lipis.dev/flags/4x3/cy.svg',
+                          fit: BoxFit.cover,
+                        )),
+                  ));
+            } else {
+              return Text('wait 1 min ');
+            }
+          },
+        ));
   }
 }
